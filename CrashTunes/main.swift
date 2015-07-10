aabe9e9b28448dd0be799cae8bc526f5fc23b456
craigomac <craigthebrit@gmail.com>
2015-07-10 15:46:06 +0200

    Initial Commit

---
+++ CrashTunes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CrashTunes
+//
+//  Created by Craig on 10/07/2015.
+//  Copyright (c) 2015 Mobiquity. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
